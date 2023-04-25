6da1b040528ce28f3a98994c7dda2b36b0cd230a
marian <82874011+DiabolicWitch@users.noreply.github.com>
2023-04-25 16:35:44 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Relogio from "./Components/Relogio";
 import Letreiro from "./Components/Letreiro";
+import ContadorPessoas from "./Components/Contador";
 function App() {
   return (
     <div className="App">
       <Letreiro />
       <Relogio />
+      <ContadorPessoas />
     </div>
   );
 }
