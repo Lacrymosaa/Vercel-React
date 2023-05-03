f9b98d8b854bfdc013ee68b56059c4285c61c831
marian <82874011+DiabolicWitch@users.noreply.github.com>
2023-05-03 18:02:13 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import Relogio from "./Components/Relogio";
 import Letreiro from "./Components/Letreiro";
 import ContadorPessoas from "./Components/Contador";
+import Gallery from "./Components/Gallery";
+import { Profile } from "./Components/Gallery";
+import TodoList from "./Components/TodoList";
+import AProfile from './Components/Avatar';
+import PackingList from './Components/PackingList';
+import List from './Components/ListComponent';
+import TeaGathering from './Components/tea-gathering';
 function App() {
   return (
     <div className="App">
       <Letreiro />
       <Relogio />
       <ContadorPessoas />
+      <Gallery />
+      <Profile />
+      <TodoList />
+      <AProfile />
+      <PackingList />
+      <List />
+      <TeaGathering />
     </div>
   );
 }
