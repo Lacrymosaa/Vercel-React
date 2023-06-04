21cc87015c63af42e3c754254ee445a6f4a13ff0
marian <82874011+DiabolicWitch@users.noreply.github.com>
2023-06-04 10:53:51 -0300

    Add files via upload

--- src/components/atividade04/Atividade04.js
+++ src/components/atividade04/Atividade04.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import {Link} from "react-router-dom"
 
+import '../../css/home/home.css';
+
 export default function Atividade04 () {
     return (
-        <div>
+        <div className="container-main">
             <nav>
                 <ul>
+                    <li>
+                        <h1>Atividade 04</h1>
+                    </li>
                     <li>
                         <Link to='/Toolbar'>Toolbar</Link>
                     </li>
